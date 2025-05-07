26a5352c579d27cc614e063d91bcad0b81f74cad
gavcx <gavlemons@gmail.com>
2025-05-07 14:16:11 -0700

    ver 0.0.3

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,18 +1,18 @@
 extends CharacterBody2D
 
 #movement constants
-const SPEED = 250.0
+const SPEED = 200.0
+const CROUCH_SPEED = 125.0
 const GROUND_ACCELERATION = 5000.0
 const AIR_ACCELERATION = 1000.0
 const GROUND_FRICTION = 3500.0
 const AIR_FRICTION = 500.0
-const CROUCH_SPEED_REDUCTION = 0.5
 const GRAVITY = 1500
 
 #jump constants
 const JUMP_BUFFER_WINDOW = 0.2
 const COYOTE_TIME_WINDOW = 0.1
-const CHARGE_JUMP_MAX_TIME = 2.0
+const CHARGE_JUMP_MAX_TIME = 1.8
 const CHARGE_JUMP_MIN_TIME = 0.3
 const GROUND_JUMP_VELOCITY = -600.0
 const COYOTE_JUMP_VELOCITY = -700.0
@@ -26,6 +26,7 @@ const CHARGED_JUMP_VELOCITY = -850.0
 #variables
 var is_crouching := false
 var is_charging_jump := false
+var is_fully_charged := false
 var coyote_timer: float = 0.0
 var jump_buffer_timer: float = 0.0
 var charge_jump_timer := 0.0
@@ -63,7 +64,8 @@ func _physics_process(delta: float) -> void:
 		stand()
 		
 func update_timers(delta: float) -> void:
-	
+	var direction := Input.get_axis("move_left", "move_right")
+		
 	if is_on_floor():
 		coyote_timer = COYOTE_TIME_WINDOW
 	else:
@@ -74,7 +76,7 @@ func update_timers(delta: float) -> void:
 	else:
 		jump_buffer_timer = max(jump_buffer_timer - delta, 0)
 		
-	if is_on_floor() and is_crouching:
+	if is_on_floor() and is_crouching and direction == 0:
 		if Input.is_action_pressed("crouch") and not is_charging_jump:
 			is_charging_jump = true
 			charge_jump_timer = 0.0
@@ -83,6 +85,10 @@ func update_timers(delta: float) -> void:
 		if is_charging_jump:
 			charge_jump_timer = min(charge_jump_timer + delta, CHARGE_JUMP_MAX_TIME)
 			charge_percent = min((charge_jump_timer / CHARGE_JUMP_MAX_TIME) * 100, 100)
+			
+			if charge_percent >= 100 and not is_fully_charged:
+				is_fully_charged = true
+				animated_sprite.play("charge_full")
 			
 			if fmod(charge_percent, 10) < 0.5:
 				print("charge: ", charge_percent, "%")
@@ -104,11 +110,17 @@ func handle_movement(delta: float) -> void:
 	elif direction < 0:
 		animated_sprite.flip_h = true
 		
-	if direction != 0:
-		velocity.x = move_toward(velocity.x, direction * SPEED, acceleration * delta)
+	if !is_crouching:
+		if direction != 0:
+			velocity.x = move_toward(velocity.x, direction * SPEED, acceleration * delta)
+		else:
+			velocity.x = move_toward(velocity.x, 0, friction * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, friction * delta)
-		
+		if direction != 0:
+			velocity.x = move_toward(velocity.x, direction * CROUCH_SPEED, acceleration * delta)
+		else:
+			velocity.x = move_toward(velocity.x, 0, friction * delta)
+			
 func crouch():
 	if is_crouching:
 		return
@@ -116,7 +128,7 @@ func crouch():
 	if is_crouching:
 		standing_cshape.disabled = true
 		crouching_cshape.disabled = false
-	
+		
 func stand():
 	if is_crouching == false:
 		return
@@ -147,9 +159,10 @@ func handle_jump() -> void:
 			jump_buffer_timer = 0.0
 			coyote_timer = 0.0
 			print("crouch jump triggered | velocity: ", velocity.y) #debug
-	
-		is_charging_jump = false
+			
 		is_crouching = false
+		is_charging_jump = false
+		is_fully_charged = false
 		standing_cshape.disabled = false
 		crouching_cshape.disabled = true
 		charge_jump_timer = 0.0
@@ -158,24 +171,23 @@ func handle_jump() -> void:
 	elif is_on_floor() and velocity.y > 0:
 		velocity.y = 0
 		
-func update_animations() -> void:
-	var direction := Input.get_axis("move_left", "move_right")
+func update_animations():
+	var direction = Input.get_axis("move_left", "move_right")
 	
-	if is_on_floor():
-		if direction == 0:
-			if is_crouching:
-				animated_sprite.play("crouch_down")
-				if animated_sprite.animation_finished:
-					animated_sprite.play("crouch_idle")
-			else:
-				animated_sprite.play("idle")
+	if is_on_floor() and Input.is_action_pressed("crouch"):
+		if is_fully_charged:
+			animated_sprite.play("charge_full")
+		elif direction == 0:
+			animated_sprite.play("crouch_idle")
 		else:
-			if is_crouching:
-				animated_sprite.play("crouch_idle")
-			else:
-				animated_sprite.play("walk")
-	else:
-		if velocity.y < 0:
-			animated_sprite.play("jump")
-		elif velocity.y > 0:
-			animated_sprite.play("fall")
+			animated_sprite.play("crouch_walk")
+			
+	elif is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("walk", direction != 0)
+			
+	if not is_on_floor():
+		animated_sprite.play("jump", velocity.y <= 0)
+		animated_sprite.play("fall", velocity.y >= 0)
